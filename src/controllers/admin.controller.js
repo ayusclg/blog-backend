390ffeb1a903896e3945a84260d5fdd797638ef9
ayusclg <freelancerayus@gmail.com>
2025-02-22 21:03:58 +0545

    Admin Can Fetch User

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -9,7 +9,7 @@ const deleteUser = async function (req,res){
                 message:"Youre Not Allowed"
             })
         }
-        console.log(req.params._id)
+
         await User.deleteOne({_id:req.params._id})
         res.status(200).json({
             message:"Successfully deleted"
@@ -21,4 +21,47 @@ const deleteUser = async function (req,res){
         })
     }
 }
-export {deleteUser}
+
+const fetchAllUser = async function (req,res){
+    try {
+        const user = await User.findById(req.user._id).select("-password -refresh_token")
+        if(user.role !== "admin"){
+            return res.status(403).json({
+                message:"Access Denied"
+            })
+        }
+
+        let page = parseInt(req.query.page) || 1
+        let perPage = parseInt(req.query.perPage) || 3
+        let role = req.query.role 
+
+        let fetchUser = {}
+        if(role)
+        fetchUser.role = role
+
+        const allUser = await User.find(fetchUser).select("-password -refresh_token")
+        .skip((page-1)*perPage)
+        .limit(perPage)
+
+        if(!allUser){
+            return res.status(400).json({
+                message:"No User Found"
+            })
+        }
+        const totalUser = await User.countDocuments(fetchUser)
+
+        res.status(200).json({
+            message:"User fetched Successfully",
+            page:page,
+            perPage:perPage,
+            TotalUser:totalUser,
+            details:allUser
+        })
+        }
+     catch (error) {
+        res.status(500).json({
+            message:"unsucessfull"
+        })
+    }
+}
+export {deleteUser,fetchAllUser}
--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,9 +1,10 @@
 import Router from "express";
-import { deleteUser } from "../controllers/admin.controller.js";
+import { deleteUser, fetchAllUser } from "../controllers/admin.controller.js";
 import { verifyToken } from "../middlewares/auth.middlewares.js";
 
 const router = Router()
 
 router.route("/del/:_id").delete(verifyToken,deleteUser)
+router.route("/fetch").get(verifyToken,fetchAllUser)
 
 export default router
